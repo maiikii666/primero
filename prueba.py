34f04a8d9cc3d2926760b9a767948185857bd75b
maiikii666 <maiikii@hotmail.com>
2021-04-30 12:51:01 -0500

    primer ejemplo

---
+++ prueba.py
@@ -0,0 +1,14 @@
+print("Hola Mundo")
+print("Bienvenido, esta es una calculadora de sumas :), escribe tu nombre")
+nombre=input()
+print("Hola "+nombre+", introduce un número")
+numA= float(input())
+print("¿Cuanto le quieres sumar?")
+numB= float(input())|
+print("Calculadora de sumas")
+total=numA+numB
+print(nombre+", tu resultado es "+str(total)+", Adios.")
+
+
+
+#no estoy seguro de como se comenta. pero espero salga bien
