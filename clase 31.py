9cf403262f8b722660c0477def0e25f64a64e718
maiikii666 <maiikii@hotmail.com>
2021-05-03 15:10:33 -0500

    prueba 3

---
+++ clase 31.py
@@ -0,0 +1,8 @@
+numeroUno=int(input("Digíta el primer número: "))
+
+numeroDos=int(input("Digíta el segundo número: "))
+
+if numeroUno<numeroDos:
+    print("El número ",numeroDos,"es mayor que ",numeroUno)
+else:
+    print("El número ",numeroUno,"es mayor que ",numeroDos)
