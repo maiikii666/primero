fe07f2393d76fd1b4c19fd693cad2f4553ff8086
maiikii666 <maiikii@hotmail.com>
2021-05-03 20:46:33 -0500

    con condicional largo

--- Retos/reto1.py
+++ Retos/reto1.py
@@ -1,12 +1,16 @@
-temperaturaMediaAnual=float(input("Digite la Temperatura Media Anual: "))
-precipitacionAnual=float(input("Digite el valor de la precipitación Anual: "))
+tMA=float(input("Digite la Temperatura Media Anual: "))
+pMA=float(input("Digite el valor de la precipitación Anual: "))
 
-if temperaturaMediaAnual>=76 and temperaturaMediaAnual<=82:
-    if precipitacionAnual>=6.0 and precipitacionAnual<=8.4:
-        print("Sumamente Apto")
-elif ((temperaturaMediaAnual>=83 and temperaturaMediaAnual<=86) or (temperaturaMediaAnual>=75 and temperaturaMediaAnual<=69)) and ((precipitacionAnual>=8.5 and precipitacionAnual<=10.4) or (precipitacionAnual>=5.0 and precipitacionAnual<=5.9)):
-    print("Moderadamente Apto")
-elif ((temperaturaMediaAnual>=87 and temperaturaMediaAnual<=90) or (temperaturaMediaAnual>=64 and temperaturaMediaAnual<=68)) and ((precipitacionAnual>=10.5 and precipitacionAnual<=12.5) or (precipitacionAnual>=4.0 and precipitacionAnual<=4.99)):
-    print("Marginalmente Apto")
-else:
+if tMA<64 or tMA>90 or pMA<4.0 or pMA>12.5:
     print("No Apto")
+elif (tMA>76 and tMA<82) and (pMA>6 and pMA<8.4):
+    print("Sumamente Apto")
+else:
+    if ((tMA>83 and tMA<86) or (tMA>69 and tMA<75)) and ((pMA>8.5 and pMA<10.4) or (pMA>5 and pMA<5.9)):
+        print("Moderadamente Apto")
+    elif ((tMA>76 and tMA<82) and ((pMA>8.5 and pMA<10.4) or (pMA>5 and pMA<5.9))) or ((pMA>6 and pMA<8.4) and ((tMA>83 and tMA<86) or (tMA>69 and tMA<75))):
+        print("Moderadamente Apto")
+    elif ((tMA>87 and tMA<90) or (tMA>64 and tMA<68)) and ((pMA>10.5 and pMA<12.5) or (pMA>4 and pMA<4.99)):
+        print("Marginalmente Apto")
+    else:
+        print("Marginalmente Apto raro")
