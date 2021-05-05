b03586d459f76935c39c35436b4291e34d4f8d00
maiikii666 <maiikii@hotmail.com>
2021-05-05 14:23:48 -0500

    normalio

--- Retos/reto1.py
+++ Retos/reto1.py
@@ -1,10 +1,10 @@
 tMA=float(input("Digite la Temperatura Media Anual "))
 pMA=float(input("Digite el valor de la precipitacion Anual "))
 
-if (tMA>64 and tMA<90) and (pMA>4 and pMA<12.5):
-    if ((tMA>87 and tMA<90) or (tMA>64 and tMA<68)) or ((pMA>10.5 and pMA<12.5) or (pMA>4 and pMA<4.99)):
+if (64<tMA<90) and (4<pMA<12.5):
+    if ((87<tMA<90) or (64<tMA<68)) or ((10.5<pMA<12.5) or (4<pMA<4.99)):
         print("Marginalmente apto")
-    elif ((tMA>83 and tMA<86) or (tMA>69 and tMA<75)) or ((pMA>8.5 and pMA<10.4) or (pMA>5 and pMA<5.9)):
+    elif ((83<tMA<86) or (69<tMA<75)) or ((8.5<pMA<10.4) or (5<pMA<5.9)):
         print("Moderadamente apto")
     else:
         print("Sumamente apto")
---
+++ clases/clase4.py
@@ -0,0 +1,5 @@
+tempe=float(input("Digite un valor: "))
+
+if 85<=tempe<95:
+    print("Está en el rango")
+
